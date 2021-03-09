89a77d319be7c1cee7e0e354d2237e0c67e01d6d
Jang7859 <wodus554@naver.com>
2021-03-09 15:31:15 +0900

    Initial Commit

---
+++ Algorithm/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Algorithm
+//
+//  Created by 장재연 on 2021/03/09.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
