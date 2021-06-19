1cb84993a0e6c13c0f3071ba7fbd91cce54ac0d3
Jang7859 <wodus554@naver.com>
2021-06-19 17:47:59 +0900

    me

--- Algorithm/Sort_merge.c
+++ Algorithm/Sort_merge.c
@@ -1,24 +1,4 @@
-////
-////  main.c
-////  Algorithm
-////
-////  Created by 장재연 on 2021/03/09.
-////
 //
-<<<<<<< HEAD
-//#include <stdio.h>
-//#include <math.h>
-//#include <stdlib.h>
-//#define SIZE 10
-//void Merge(int arr[], int left, int mid, int right){
-//    int i, j, k=0;
-//    i=left; j=mid+1;
-//    int *temparr = (int*)malloc(sizeof(int)*(right-left+1));
-//
-//    while(i<=mid&&j<=right){
-//        if(arr[i]<arr[j]) temparr[k++] = arr[i++];
-//        else temparr[k++] = arr[j++];
-=======
 //  main.c
 //  Algorithm
 //
@@ -69,48 +49,13 @@ void MergeSort(int arr[], int left, int right){
 //    int arr[SIZE] ={6,45,3,8,30,6,1,58,23,96};
 //    for(int i=0;i<SIZE;i++){
 //        printf("%d ", arr[i]);
->>>>>>> 476e400c2739db537c6e14347b9a9826468c1dec
 //    }
-//
-//    while(i<=mid)temparr[k++] = arr[i++];
-//    while(j<=right) temparr[k++] = arr[j++];
-//    k--;
-//
-//    while(k>=0){
-//        arr[left+k] = temparr[k];
-//        k--;
+//    printf("\n");
+//    MergeSort(arr,0, SIZE-1);
+//    for(int i=0;i<SIZE;i++){
+//        printf("%d ", arr[i]);
 //    }
+//    printf("\n");
 //
+//    return 0;
 //}
-//
-//void MergeSort(int arr[], int left, int right){
-//    if(left<right){
-//        int mid = left + (right-left)/2;
-//
-//        MergeSort(arr, left, mid);
-//        MergeSort(arr, mid+1, right);
-//
-//        Merge(arr, left, mid, right);
-//
-//    }
-//}
-//
-//
-//
-//
-////int main(void){
-////
-////    int arr[SIZE] ={6,45,3,8,30,6,1,58,23,96};
-////    for(int i=0;i<SIZE;i++){
-////        printf("%d ", arr[i]);
-////    }
-////    printf("\n");
-////    MergeSort(arr,0, SIZE-1);
-////    for(int i=0;i<SIZE;i++){
-////        printf("%d ", arr[i]);
-////    }
-////    printf("\n");
-////
-////    return 0;
-////}
-//
